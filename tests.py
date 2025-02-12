e933f48ff68fe114bfb33776a4704b488be44cc3
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-12 09:31:31 -0600

    Add 2 new tests for add_surf_boards

--- tests.py
+++ tests.py
@@ -6,8 +6,14 @@ class SamTests(unittest.TestCase):
     def setUp(self):
         self.cart = surfshop.ShoppingCart()
 
-    def test_add_surf_boards(self):
-        self.assertEquals(self.cart.add_surfboards(1), "Successfully added 1 surfboard to cart!")
+    def test_add_surf_boards_one(self):
+        self.assertEqual(self.cart.add_surfboards(1), "Successfully added 1 surfboard to cart!")
+    
+    def test_add_surf_boards_two(self):
+        self.assertEqual(self.cart.add_surfboards(2), "Successfully added 2 surfboards to cart!")
 
+    def test_add_surf_boards_five(self):
+        with self.assertRaises(surfshop.TooManyBoardsError):
+            self.cart.add_surfboards(5)
 
 unittest.main()
