4f411180781d840412499a6a308976cc6f1240f1
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-12 08:59:49 -0600

    Test add_surf_boards

--- tests.py
+++ tests.py
@@ -5,3 +5,9 @@ class SamTests(unittest.TestCase):
     
     def setUp(self):
         self.cart = surfshop.ShoppingCart()
+
+    def test_add_surf_boards(self):
+        self.assertEquals(self.cart.add_surfboards(1), "Successfully added 1 surfboard to cart!")
+
+
+unittest.main()
